9d21f558b781daf36dc9ba720d7ea09bf9762914
‘Anastasiia <miss.iuferova@yandex.ru>
2022-09-06 19:50:16 +0200

    feat: Main section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Main.css"
 
 function Main() {
     return (
-        <div > 
+        <div className="main"> 
+            <div className="main__container">
+                <h1 className="main__title">TODO</h1>
+                <p className="main__subtitle">Simple to do list app design</p>
+                <NavLink to="/todo"><button className="main__link">Explore</button></NavLink>
+            </div>
         </div>
     );
 }
