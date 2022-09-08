4690808df4b133f9c151574a84e367abf6360b7c
‘Anastasiia <miss.iuferova@yandex.ru>
2022-09-08 17:36:47 +0200

    feat: TaskInput css+html

---
+++ src/components/TaskInput/TaskInput.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./TaskInput.css"
+
+function TaskInput() {
+
+    return (
+        <div className="task-input"> 
+            <div className="task-input__container">
+                <form className="task-input__container-form" >
+                    <input  className="task-input__input" placeholder="Create a new todo..." type="text"></input>
+                    <button className="task-input__button" type="submit"></button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default TaskInput;
+
+
+
--- src/components/ToDoPage/ToDoPage.js
+++ src/components/ToDoPage/ToDoPage.js
@@ -1,11 +1,13 @@
 import React from "react";
-import "./ToDoPage.css"
+import "./ToDoPage.css";
+import TaskInput from "../TaskInput/TaskInput";
 
 function ToDoPage() {
     return (
         <div className="td-page"> 
             <div className="td-page__container">
                 <h1 className="td-page__title">TODO</h1>
+                <TaskInput />
             </div>
         </div>
     );
