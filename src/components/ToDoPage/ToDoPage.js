1ac8d8dd7bc835fa39e3dc1a6ea906dda24d6758
‘Anastasiia <miss.iuferova@yandex.ru>
2022-09-13 17:01:49 +0200

    feat: add Resize

--- src/components/ToDoPage/ToDoPage.js
+++ src/components/ToDoPage/ToDoPage.js
@@ -1,14 +1,46 @@
 import React from "react";
 import "./ToDoPage.css";
+import { useCallback, useEffect, useState} from "react";
+import { useDebouncedCallback } from 'use-debounce';
 import TaskInput from "../TaskInput/TaskInput";
-import TaskList from "../TaskList/TaskList"
+import TaskList from "../TaskList/TaskList";
+
 
 function ToDoPage() {
+
+    const [isInputVisible, setInputVisible] = useState(true)
+
+    const Resize = useCallback(
+        (width) => {
+            if (width > 488) {
+                setInputVisible(true)
+            }
+            else setInputVisible(false)
+        }, []
+    )  //general function
+
+    useEffect(() => {
+        Resize(window.innerWidth)
+    }, [Resize]) // initial rendering
+
+    const dynamicResize = useDebouncedCallback(
+        () => {
+            Resize(window.innerWidth)
+        }, 200
+    ) 
+
+    useEffect(() => {
+        window.addEventListener('resize', dynamicResize)
+        return () => {
+                window.removeEventListener('resize', dynamicResize);
+        } 
+    }, [dynamicResize]) // when changing window width
+
     return (
         <div className="td-page"> 
             <div className="td-page__container">
                 <h1 className="td-page__title">TODO</h1>
-                <TaskInput />
+                {isInputVisible && <TaskInput /> }
                 <TaskList />
             </div>
         </div>
