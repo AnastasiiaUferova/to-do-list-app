6848a6151c593dee0654a050e98ad5d812ba277c
‘Anastasiia <miss.iuferova@yandex.ru>
2022-09-20 19:40:26 +0200

    feat: add Form.js

---
+++ src/components/AddForm/AddForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Form from "../Form/Form";
+
+export default function AddForm() {
+    return (
+        <Form name="add-form" title="Edit task">
+            <button className="popup__cancel-button" ></button>
+        </Form>
+    );
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import "../../index.css";
 import Main from '../Main/Main';
 import ToDoPage from "../ToDoPage/ToDoPage";
 import { Route, Routes } from "react-router-dom";
+import AddForm from "../AddForm/AddForm";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Main />}></Route>
       <Route path="/todo" element={< ToDoPage/>}></Route>
       </Routes>
+      <AddForm />
     </div>
   );
 }
