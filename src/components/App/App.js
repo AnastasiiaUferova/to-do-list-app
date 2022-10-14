24da1e8a8014361c72dd8897e15dc687941fa491
‘Anastasiia <miss.iuferova@yandex.ru>
2022-10-14 17:59:47 +0200

    feat: add useClickOutside hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect} from "react";
 import "./App.css";
 import "../../index.css";
 import Main from '../Main/Main';
@@ -8,38 +8,26 @@ import { Route, Routes } from "react-router-dom";
 import AddForm from "../AddForm/AddForm";
 import EditForm from "../EditForm/EditForm";
 import Rewards from "../Rewards/Rewards";
+import useClickOutside from "../../hooks/useClickOutside";
+
 
 
 function App() {
 
 
-  const [tasks, setTasks] = useState(
+const [tasks, setTasks] = useState(
     () => JSON.parse(localStorage.getItem("tasks")) || []
   );
 
-  useEffect(() => {
+useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
-  const [isAddFormOpen, setIsAddFormOpen] = useState(false);
+const [isAddFormOpen, setIsAddFormOpen] = useState(false);
 
-  let ref = useRef();
-
-  useEffect(() => {
-    const checkIfClickedOutside = event => {
-        if(isAddFormOpen && ref.current && !ref.current.contains(event.target)) {
-          setIsAddFormOpen(false);
-        }
-    }
-    document.addEventListener("mousedown", checkIfClickedOutside)
-
-    return () => {
-      // Cleanup the event listener
-      document.removeEventListener("mousedown", checkIfClickedOutside)
-    }
-  }, [isAddFormOpen])
-
-
+let ref = useClickOutside(()=> {
+  setIsAddFormOpen(false);
+})
 
   function addTask(task) {
     setTasks([...tasks, task]);
